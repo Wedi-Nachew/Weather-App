ac239641dd97b3bd92e8988816755697ef2150b3
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-14 10:35:01 +0300

    Write a function that returns the current & forecast weather of a city

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "./style.css";
-console.log("hii");
+// key=75d0fc888d5f466b885144654231108
+const getCurrentWeather = async (city = "Seattle") => {
+    const [currentWeatherResponse, forecastWeatherResponse] = await Promise.all(
+        [
+            fetch(
+                `https://api.weatherapi.com/v1/current.json?key=75d0fc888d5f466b885144654231108&q=${city}&aqi=no`,
+                { mode: "cors" }
+            ),
+            fetch(
+                `https://api.weatherapi.com/v1/forecast.json?key=75d0fc888d5f466b885144654231108&q=${city}&days=6&aqi=no&alerts=no`,
+                { mode: "cors" }
+            ),
+        ]
+    );
+
+    const [currentWeather, forecastWeather] = await Promise.all([
+        currentWeatherResponse.json(),
+        forecastWeatherResponse.json(),
+    ]);
+
+    console.log(currentWeather, forecastWeather);
+};
+// city = prompt("Enter city name: ");
+getCurrentWeather("denver");
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
+    devtool: "inline-source-map",
     output: {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, "./dist"),
