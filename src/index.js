2cba3aa03d78a940c0098d81d7e6bb995f261dd2
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-14 12:27:06 +0300

    Wrap icons & text titles in a div element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css";
 // key=75d0fc888d5f466b885144654231108
+// "ᵒC"
 const getCurrentWeather = async (city = "Seattle") => {
     const [currentWeatherResponse, forecastWeatherResponse] = await Promise.all(
         [
@@ -8,7 +9,7 @@ const getCurrentWeather = async (city = "Seattle") => {
                 { mode: "cors" }
             ),
             fetch(
-                `https://api.weatherapi.com/v1/forecast.json?key=75d0fc888d5f466b885144654231108&q=${city}&days=6&aqi=no&alerts=no`,
+                `https://api.weatherapi.com/v1/forecast.json?key=75d0fc888d5f466b885144654231108&q=${city}&days=7&aqi=no&alerts=no`,
                 { mode: "cors" }
             ),
         ]
@@ -18,8 +19,17 @@ const getCurrentWeather = async (city = "Seattle") => {
         currentWeatherResponse.json(),
         forecastWeatherResponse.json(),
     ]);
+    const currentCondition = currentWeather.location;
 
-    console.log(currentWeather, forecastWeather);
+    console.log(currentCondition);
+    // renderWeatherInfo(currentWeather);
 };
 // city = prompt("Enter city name: ");
 getCurrentWeather("denver");
+const renderWeatherInfo = (currentWeather) => {
+    const wrapper = document.querySelector(".wrapper");
+    const currentWeatherBoard = wrapper.querySelector(".current-weather");
+
+    currentWeatherBoard.children[0].children[0];
+};
+// renderWeatherInfo();
