68fe54e32a719870134d1bba945a182fad19760a
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-14 09:50:05 +0300

    Create lay-out of the page

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-modules.export = {
+module.export = {
+    mnode: "development",
     entry: "./src/index.js",
     output: {
         filename: "[name][contenthash].js",
         path: path.resolve(__dirname, "/dist"),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "[name][contenthash].html",
+            template: "./src/index.html",
+        }),
+    ],
 };
