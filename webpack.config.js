863a78774ec36cdb76274e8edcf635b5b9864ac7
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-14 10:12:39 +0300

    Install html-loader to resolve img files paths

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,31 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.export = {
-    mnode: "development",
+module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "[name][contenthash].js",
-        path: path.resolve(__dirname, "/dist"),
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "./dist"),
+        assetModuleFilename: "images/[name].[ext]",
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(svg|jpeg|jpg|png|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.html/i,
+                use: "html-loader",
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: "[name][contenthash].html",
             template: "./src/index.html",
+            filename: "index.[contenthash].html",
         }),
     ],
 };
